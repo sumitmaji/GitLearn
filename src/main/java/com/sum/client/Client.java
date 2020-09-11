def34ad563194daddf2437f4bb278c23fc0b703a
Sumit Maji <skmaji1@outlook.com>
2020-09-11 09:52:40 +0530

    Feature Commit #2

--- src/main/java/com/sum/client/Client.java
+++ src/main/java/com/sum/client/Client.java
@@ -1,6 +1,7 @@
 package com.sum.client;
 public class Client {
     public static void main(String[] args) {
-        System.out.println("Code commited from feature branch");
+
+        System.out.println("Code commited from feature branch, this is good");
     }
 }
