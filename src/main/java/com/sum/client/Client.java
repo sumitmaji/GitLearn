6f73e3dd581a8c9d125bf20b63cf98e8e2b12010
Sumit Maji <skmaji1@outlook.com>
2020-09-11 11:08:53 +0530

    Feature Commit #3

--- src/main/java/com/sum/client/Client.java
+++ src/main/java/com/sum/client/Client.java
@@ -2,6 +2,6 @@ package com.sum.client;
 public class Client {
     public static void main(String[] args) {
 
-        System.out.println("Code commited from feature branch, this is good");
+        System.out.println("Code commited from feature branch, this is good, we need to merge it with master");
     }
 }
